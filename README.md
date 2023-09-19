cd02fa0b833e063892f392e0bdc2254ad575f55e
benjo <130617538+benjo00@users.noreply.github.com>
2023-09-19 15:59:36 -0500

    completely rework entire site for practice on the Odin Project

---
+++ README.md
@@ -0,0 +1,2 @@
+# odin-recipes
+A website that contains recipes and links to them. Update: The Odin Project said to go back and soup up this website. I don't really care about recipes, so I remade it to be about philosophy. Enjoy.
---
+++ script.js
@@ -0,0 +1,63 @@
+
+
+let input = prompt("What is the meaning of life?")
+if (input == "To party" || input == "to party" || input == "To party!" || input == "to party!") {
+    alert("You're an animal!");
+} else if (input == "forty-two" || input == "forty two" || input == "42") {
+    alert("Ah, that is the ultimate answer, but what is the ultimate question?");
+} else if (input == "love") {
+    alert("Awwwww.");
+} else if (input == "feet") {
+    alert("Weird!");
+} else if (input == "sirens") {
+    alert("okay Homer");
+} else if (input == "goblins") {
+    alert("okay so dnd?");
+} else if (input == "dracula") {
+    alert("the ravings of a demented Irishman.");
+} else if (input == "some kind of potato man") {
+    alert("the lord of all things.");
+} else if (input == "squirt") {
+    alert("Amrita, nectar of the gods, a delicious grapefruit soda!");
+} else if (input == "peace") {
+    alert("good answer.");
+} else if (input == "eggs") {
+    alert("I mean, they're pretty good, but, the 'best thing?' No.");
+} else if (input == "food") {
+    alert("No arguments here!");
+} else if (input == "salmon") {
+    alert("Yum.");
+} else if (input == "pasta") {
+    alert("Yes, but with a Mario-like accent.");
+} else if (input == "water") {
+    alert("I mean, yeah, not very imaginitive, but technically correct.");
+} else if (input == "Love") {
+    alert("Well, isn't that a nice answer?");
+} else if (input == "sleep") {
+    alert("Got it, go talk to morpheus.");
+} else if (input == "Dune" || input == "dune" || input == "Dune." || input == "dune.") {
+alert("The spice must flow.");
+} else if (input == "beauty") {
+    alert("Go see if Narcissus has any openings.");
+} else if (input == "art") {
+    alert("Where's that muse?");
+} else if (input == "books") {
+    alert("Yeah, good answer, but what genre? Cuz if you only read textbooks on different types of soil, you need to get out more.");
+} else if (input == "animals") {
+    alert("Man is a animal. -Aristotle misquoted");
+} else if (input == "people") {
+    alert("No. I mean some are pretty cool, but overall, no.");
+} else if (input == "music") {
+    alert('"The great communicator, use two sticks to make it in the nature!"');
+} else if (input == "nature") {
+alert("Yay neature!");
+} else if (input == "big flopping donkey ears.") {
+alert("Why are you like this?");
+} else if (input == "What is the ultimate question?") {
+    alert("What do you get when you multiply six by seven?");
+}
+else {
+    alert("On second thought, let’s not go to Camelot. ‘Tis a silly place");
+}
+
+
