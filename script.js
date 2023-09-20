9e81f6b3c778785129e1b502038bcc8858a5287b
benjo <130617538+benjo00@users.noreply.github.com>
2023-09-19 19:38:30 -0500

    add yet more text.

--- script.js
+++ script.js
@@ -55,8 +55,29 @@ alert("Yay neature!");
 alert("Why are you like this?");
 } else if (input == "What is the ultimate question?") {
     alert("What do you get when you multiply six by seven?");
-}
-else {
+} else if (input == "golf") {
+    alert("Hole in one!");
+} else if (input == "sports") {
+    alert("Go team!");
+} else if (input == "god") {
+    alert("How pious.");
+} else if (input == "video games") {
+    alert("well said");
+} else if (input == "existing") {
+    alert("You're not wrong!");
+} else if (input == "to live") {
+    alert("“The meaning of life is just to be alive. It is so plain and so obvious and so simple. And yet, everybody rushes around in a great panic as if it were necessary to achieve something beyond themselves -Alan Watts");
+} else if (input == "there is no meaning") {
+    alert("Ah, nihilism.");
+} else if (input == "to learn" || input == "learning") {
+    alert("Lao Tzu approves");
+} else if (input == "to reproduce") {
+    alert("That's arguably correct on a technical level.");
+} else if (input == "i don't know" || input == "I don't know" || input == "idk") {
+    alert("How very Ajnana of you.");
+} else if (input == "to pass on knowledge" || input == "To pass on knowledge") {
+    alert("Find something to believe in, and find it for yourself. When you do, pass it on to the future. -Solid Snake");
+} else {
     alert("On second thought, let’s not go to Camelot. ‘Tis a silly place");
 }
 
